49b42c54504b927bee5d36789ae56964d1ab6559
Jakub Folta <jakubfolta@yahoo.co.uk>
2024-02-12 11:47:48 +0000

    Remove hardcoded Django secret key

--- demo/settings.py
+++ demo/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u(@co06=s+#@+tz)p5uw1dkde#dpgcr3sb&v*a)&meakv)(m5w'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
