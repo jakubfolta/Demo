8e96900643a983f0b1a3c8991ded6b1b5fec3566
Jakub Folta <jakubfolta@yahoo.co.uk>
2024-02-12 12:51:46 +0000

    Update

--- demo/settings.py
+++ demo/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import django_on_heroku
 
 from pathlib import Path
 
@@ -118,5 +119,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_on_heroku
+
 django_on_heroku.settings(locals())
